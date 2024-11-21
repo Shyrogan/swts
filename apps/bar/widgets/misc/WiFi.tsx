466f1ac11f33ae7c1ca4e7049902c24b4ee3c603
Sébastien <sebastien@shyrogan.fr>
2024-11-21 19:58:57 +0100

    feat: make a floating style rather than fixed

--- apps/bar/widgets/misc/WiFi.tsx
+++ apps/bar/widgets/misc/WiFi.tsx
@@ -9,8 +9,6 @@ export default function WiFi({ label }: Props) {
   const network = Network.get_default()
   const wifi = bind(network, "wifi")
 
-  console.log(wifi.get())
-
   return <box className="wifi">
     <icon icon={wifi.as((w) => w?.get_icon_name())}/>
     <label visible={label} label={wifi.as((w) => w?.get_ssid() || "")} />
